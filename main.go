3ab1c3388f6a660a8df66f027616481cfc2e7380
Amey Jain <amey@nanonets.com>
2024-08-12 07:42:34 +0530

    go methods and receivers

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/amey-tech/learn-go/methods"
+)
+
+func main() {
+	fmt.Println("Methods in Go:")
+
+	fmt.Println("\nIntroduction-")
+	methods.DemoImplementationMethodsIntroduction()
+
+	fmt.Println("\nInterfaces-")
+	methods.DemoImplementationMethodsAndInterface()
+}
